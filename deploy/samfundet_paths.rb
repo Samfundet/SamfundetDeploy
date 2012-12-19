a6ada70190f2e4b03408aa598656ce88d8c98ea3
Jonas Amundsen <jonasba@gmail.com>
2012-12-19 21:52:44 +0100

    Project root is configurable, default to current directory.

--- deploy/samfundet_paths.rb
+++ deploy/samfundet_paths.rb
@@ -1,11 +1,11 @@
 module SamfundetPaths
-  def gem_paths
+  def gem_paths(project_root = Dir.pwd)
     ruby_v = Gem::ConfigMap[:ruby_version]
 
     {
-        :gem_home => File.expand_path("vendor/ruby/#{ruby_v}", Dir.pwd),
-        :gem_path => File.expand_path("vendor/ruby/#{ruby_v}", Dir.pwd),
-        :gem_bin  => File.expand_path("vendor/ruby/#{ruby_v}/bin", Dir.pwd)
+        :gem_home => File.expand_path("vendor/ruby/#{ruby_v}", project_root),
+        :gem_path => File.expand_path("vendor/ruby/#{ruby_v}", project_root),
+        :gem_bin  => File.expand_path("vendor/ruby/#{ruby_v}/bin", project_root)
     }
   end
 end
