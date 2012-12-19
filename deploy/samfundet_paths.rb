375d75384e53842209df6f16fb41c05d6dd2aac9
Jonas Amundsen <jonasba@gmail.com>
2012-12-19 21:51:55 +0100

    Current directory is regarded as project root.

--- deploy/samfundet_paths.rb
+++ deploy/samfundet_paths.rb
@@ -3,9 +3,9 @@ module SamfundetPaths
     ruby_v = Gem::ConfigMap[:ruby_version]
 
     {
-        :gem_home => File.expand_path("vendor/ruby/#{ruby_v}", __FILE__),
-        :gem_path => File.expand_path("vendor/ruby/#{ruby_v}", __FILE__),
-        :gem_bin  => File.expand_path("vendor/ruby/#{ruby_v}/bin", __FILE__)
+        :gem_home => File.expand_path("vendor/ruby/#{ruby_v}", Dir.pwd),
+        :gem_path => File.expand_path("vendor/ruby/#{ruby_v}", Dir.pwd),
+        :gem_bin  => File.expand_path("vendor/ruby/#{ruby_v}/bin", Dir.pwd)
     }
   end
 end
