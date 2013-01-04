69f0818043f3f11aeec40d5f1ee9ebcfbe58f6c5
Jonas Amundsen <JonasBA@Gmail.com>
2013-01-04 16:17:45 +0100

    Updating everything w/u a clean repository fails.
    The task for updating everything will execute a task which fails if the
    repository is empty, ei. git status returns anything other than an empty
    message. This ensures that nothing gets updated if the repository is
    dirty.

--- deploy/task_definitions.rb
+++ deploy/task_definitions.rb
@@ -2,6 +2,17 @@ env :GEM_PATH => gem_paths[:gem_path],
     :GEM_HOME => gem_paths[:gem_home],
     :PATH => "#{ENV['PATH']}:#{gem_paths[:gem_bin]}"
 
+task_group :git_status, :hidden => true do
+  task "Ensuring empty git status" do
+    empty_git_status = <<-MSG.gsub(/^\s+/, '')
+      # On branch master
+      nothing to commit (working directory clean)
+    MSG
+
+    raise "Repository is not clean." unless `git status` == empty_git_status
+  end
+end
+
 task_group :pull do
   description "Pull recent changes from Git remote"
 
@@ -37,6 +48,7 @@ end
 task_group :update_everything do
   description "All of the above"
 
+  invoke_group :git_status
   invoke_group :pull
   invoke_group :install_gems
   invoke_group :compile_assets
